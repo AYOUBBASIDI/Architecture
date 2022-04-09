78e3977e34a6c91d4355e97dac2e7e92a9441fc6
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2022-04-09 15:33:27 +0000

    Add files via upload

--- atdev.sql
+++ atdev.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 08, 2022 at 03:33 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 28 mars 2022 à 16:48
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,145 +18,111 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `atdev`
+-- Base de données : `atdev`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Structure de la table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
   `nom_admin` varchar(255) COLLATE utf8_bin NOT NULL,
   `prenom_admin` varchar(255) COLLATE utf8_bin NOT NULL,
   `tel_admin` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `email_admin` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `psseudo_admin` varchar(255) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `psseudo_admin` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `admin`
+-- Déchargement des données de la table `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `nom_admin`, `prenom_admin`, `tel_admin`, `email_admin`, `psseudo_admin`) VALUES
-(1, 'bsd', 'ayoub', '0652086766', 'bsd-ayoub@gmail.com', 'aaaa'),
-(22, 'jkhiom', 'uohoi', 'iohoi', 'uhuho', 'hhhh');
+(1, 'bsd', 'ayoub', '0652086766', 'bsd-ayoub@gmail.com', ''),
+(2, 'lambaraa', 'abdellah', '0652086766', 'abdellah-lamb@gmail.com', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `client`
+-- Structure de la table `client`
 --
 
-CREATE TABLE `client` (
-  `id` int(11) NOT NULL,
-  `fname` varchar(255) COLLATE utf8_bin NOT NULL,
-  `lname` varchar(255) COLLATE utf8_bin NOT NULL,
-  `tel` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(255) COLLATE utf8_bin NOT NULL,
+  `prenom_client` varchar(255) COLLATE utf8_bin NOT NULL,
+  `dateNaissance` date DEFAULT NULL,
+  `tel_client` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `profession` varchar(255) COLLATE utf8_bin NOT NULL,
-  `psseudo_client` varchar(255) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `psseudo_client` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `id_admin` int(11) NOT NULL,
+  PRIMARY KEY (`id_client`),
+  KEY `id_admin` (`id_admin`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `client`
+-- Déchargement des données de la table `client`
 --
 
-INSERT INTO `client` (`id`, `fname`, `lname`, `tel`, `profession`, `psseudo_client`) VALUES
-(142, 'hello', 'ZHADUA', '0638249248', 'other', '62501f1fd0ed0'),
-(141, 'ayoub', 'basidi', '0681530508', 'other', '62501ebf72648');
+INSERT INTO `client` (`id_client`, `nom_client`, `prenom_client`, `dateNaissance`, `tel_client`, `profession`, `psseudo_client`, `id_admin`) VALUES
+(1, 'bsd', 'ayoub', '2000-04-12', '0681530508', 'ingenieur', '0000000001', 1),
+(2, 'lambaraa', 'abdellah', '2000-04-12', '0652086766', 'pecheur', '0000000002', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rdv`
+-- Structure de la table `creneau`
 --
 
-CREATE TABLE `rdv` (
-  `id_r` int(11) NOT NULL,
-  `date_r` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+DROP TABLE IF EXISTS `creneau`;
+CREATE TABLE IF NOT EXISTS `creneau` (
+  `id_creneau` int(11) NOT NULL AUTO_INCREMENT,
+  `date_c` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_creneau`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `creneau`
+--
+
+INSERT INTO `creneau` (`id_creneau`, `date_c`) VALUES
+(1, 'de 10 h à 10:30h'),
+(2, 'de 10 h à 10:30h'),
+(3, ' de 14 h à 14:30h'),
+(4, ' de 15 h à 15:30h'),
+(5, ' de 16 h à 16:30h');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rdv`
+--
+
+DROP TABLE IF EXISTS `rdv`;
+CREATE TABLE IF NOT EXISTS `rdv` (
+  `id_r` int(11) NOT NULL AUTO_INCREMENT,
+  `date_r` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `sujet` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `id_creneau` varchar(255) COLLATE utf8_bin NOT NULL,
-  `id_client` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id_creneau` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  PRIMARY KEY (`id_r`),
+  KEY `id_creneau` (`id_creneau`),
+  KEY `id_client` (`id_client`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `rdv`
+-- Déchargement des données de la table `rdv`
 --
 
 INSERT INTO `rdv` (`id_r`, `date_r`, `sujet`, `id_creneau`, `id_client`) VALUES
-(37, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 27),
-(30, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 40),
-(42, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 31),
-(41, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 30),
-(39, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 33),
-(33, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 39),
-(38, '29-02-2022', 'bghit nbni ', '10h a 10:30h', 35),
-(40, '29-02-2022', 'bghit nbni ', '11h a 11:30h', 28),
-(43, '2020_09_08', 'aaaa', '11h a 11:30h', 90),
-(44, '2022-04-22', 'umyswdoihfo', '14h a 14:30h', 92),
-(45, '2022-04-15', 'uireugfoi', '11h a 11:30h', 92),
-(46, '2020_09_08', 'aaaa', '', 90),
-(47, '2020_09_08', 'aaaa', '', 90),
-(48, '2022-04-14', 'gdsiugfi', '', 92),
-(49, '2022-04-14', 'gdsiugfi', '', 92),
-(50, '2022-04-14', 'gdsiugfi', '', 92),
-(51, '2022-04-14', 'ugdsiugfui', '', 92),
-(52, '2022-04-22', '', '', 95),
-(53, '2022-04-15', 'jjjjj', '1', 142),
-(54, '2022-04-15', 'jjjjj', '1', 142),
-(55, '2022-04-15', 'jjjjj', '1', 142),
-(56, '2022-04-15', 'jjjjj', '1', 142),
-(57, '2022-04-15', 'jjjjj', '1', 142),
-(58, '2022-04-15', 'jjjjj', '1', 142),
-(59, '2022-04-15', 'jjjjj', '1', 142),
-(60, '2022-04-15', 'jjjjj', '1', 142);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indexes for table `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `rdv`
---
-ALTER TABLE `rdv`
-  ADD PRIMARY KEY (`id_r`),
-  ADD KEY `id_client` (`id_client`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `client`
---
-ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
-
---
--- AUTO_INCREMENT for table `rdv`
---
-ALTER TABLE `rdv`
-  MODIFY `id_r` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+(24, '2022-03-28 16:35:25', 'AZAZAZAAAZ', 1, 1),
+(25, '2022-03-28 16:35:25', 'ASASAAS', 2, 2);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
